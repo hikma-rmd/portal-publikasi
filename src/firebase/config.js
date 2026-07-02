6871b2cab2645960479dddc4d8fbd4132c48beb7
hikma-rmd <60900123009@uin-alauddin.ac.id>
2026-07-02 15:40:34 +0800

    fix: correct firebase storage bucket url

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB-EitcLkO-Gm9ObJ6hBEFsOurgafUhwA4",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "portal-publikasi-organisasi.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "portal-publikasi-organisasi",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "portal-publikasi-organisasi.firebasestorage.app",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "portal-publikasi-organisasi.appspot.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "516199087734",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:516199087734:web:1feae60006795960d92957"
 };
